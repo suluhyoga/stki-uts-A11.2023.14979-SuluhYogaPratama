c43942a53d487245c7747119f07137ac5b2b85b1
suluhyoga <yp7757786@gmail.com>
2025-11-09 14:43:04 +0700

    fix: added path handling for Streamlit deploy

--- app/main.py
+++ app/main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import streamlit as st
 from src.search import search_boolean, search_vector
 
